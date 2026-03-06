f333647fe47dbfc2d6ff5eb5008829f54f05e43a
newestnoob77 <gsjithin1234@gmail.com>
2026-03-06 20:18:50 +0530

    Update userAuth.Repostiory.js

--- src/features/user/userAuthentication/userAuth.Repostiory.js
+++ src/features/user/userAuthentication/userAuth.Repostiory.js
@@ -4,54 +4,25 @@ import { UserModel } from '../user.Schema.js';
 export default class UserAuthRepository{
   
     async signup(user){
-try{
 const newUser = new UserModel(user)
 await newUser.save()
 return newUser
 }
-catch(err){
-console.log(err)
-if(err instanceof mongoose.Error.ValidationError){
-    throw err;
-}
-else{
-    throw new ApplicationError("Something went wrong")
-}
-}
-    }
+    
     async login(){
-try{
 return await UserModel.findOne({email,password})
-}catch(err){
-    console.log(err)
-    throw new ApplicationError("Something went wrong")
-}
     }
 async findByEmail(email){
-try{
 return await UserModel.findOne({email})
-}catch(err){
-    console.log(err)
-    throw new ApplicationError("Something went wrong")
 }
-}
-    async logout(user,token){
-        try{
+    async logout(user,token){  
 user.tokens=user.tokens.filter(t=>t!==token)
 return await user.save();
-        }catch(err){
-            console.log(err)
-            throw new ApplicationError("Something went wrong")
-        }
     }
-    async logoutFromAll(user){
-try{
+   async logoutFromAll(user){
 user.tokens=[]
 return await user.save()
-}catch(err){
-    console.log(err)
-    throw new  ApplicationError("Something went wrong")
-}
     }
+
 
 }
